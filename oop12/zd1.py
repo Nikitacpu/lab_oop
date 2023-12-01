9a76c4ecaee437d9722b6ef511f98b6834a7d26b
nikita <Nik.zhuk.2007@bk.ru>
2023-12-01 04:03:58 +0300

    12

--- oop12/zd1.py
+++ oop12/zd1.py
@@ -2,24 +2,18 @@ class City:
     def __init__(self, name):
         self.__name = name  # Инициализируем название города
         self.__buildings = []  # Инициализируем пустой список для хранения зданий в городе
-
     @property
     def name(self):
         return self.__name  # Геттер для названия города
-
     @name.setter
     def name(self, value):
         self.__name = value  # Сеттер для названия города
-
     @property
     def buildings(self):
         return self.__buildings  # Геттер для списка зданий
-
     def add_building(self, building):
         building.city = self  # Устанавливаем связь между зданием и городом
         self.__buildings.append(building)  # Добавляем здание в список зданий города
-
-
 class Building:
     def __init__(self, street_name, house_number, base_monthly_payment):
         self.__street_name = street_name  # Инициализируем название улицы
@@ -27,23 +21,18 @@ class Building:
         self.__base_monthly_payment = base_monthly_payment  # Инициализируем базовую ежемесячную оплату
         self.__rooms = []  # Инициализируем пустой список для хранения помещений в здании
         self.__city = None  # Инициализируем город, в котором находится здание
-
     @property
     def city(self):
         return self.__city  # Геттер для города
-
     @city.setter
     def city(self, value):
         self.__city = value  # Сеттер для города
-
     @property
     def street_name(self):
         return self.__street_name  # Геттер для названия улицы
-
     @street_name.setter
     def street_name(self, value):
         self.__street_name = value  # Сеттер для названия улицы
-
     @property
     def house_number(self):
         return self.__house_number  # Геттер для номера дома
@@ -85,7 +74,6 @@ class Room:
     @area.setter
     def area(self, value):
         self.__area = value  # Сеттер для площади помещения
-
     def calculate_monthly_payment(self):
         pass  # Метод для расчета ежемесячной оплаты, который будет переопределен в подклассах
 class Apartment(Room):
@@ -98,7 +86,6 @@ class Apartment(Room):
     @tenants.setter
     def tenants(self, value):
         self.__tenants = value  # Сеттер для списка жильцов
-
     def calculate_monthly_payment(self):
         try:
             if not self.__tenants:
@@ -107,7 +94,6 @@ class Apartment(Room):
             return 10 * self.__area
         except ValueError as e:
             print(f"Произошла ошибка при расчете ежемесячной оплаты: {e}")  # Обрабатываем исключение и выводим сообщение об ошибке
-            return 0
 class Office(Room):
     def __init__(self, number, area, company_name, activity_type):
         super().__init__(number, area)  # Инициализируем номер и площадь помещения с помощью конструктора родительского класса
@@ -136,14 +122,11 @@ city = City("Минск")  # Создаем город Минск
 building = Building("Ленина", 1, 100.0)
 city.add_building(building)  # Добавляем здание в город
 building.add_room(Room(1, 50.0))
-building.add_room(Apartment(2, 75.0, ["Васильев"]))  # Добавляем в здание квартиру площадью 75.0 кв. м с жильцом Иванов И.И.
-building.add_room(Office(3, 100.0, "Офиис 1 ", "Офис2 "))
+building.add_room(Apartment(2, 75.0, ["Васильев"]))
+building.add_room(Office(3, 100.0, "Офис 2 ", "Офис 1 "))
 print(building.total_area)  # Выводим общую площадь всех помещений в здании
-
 # Создаем квартиру без жильцов
 empty_apartment = Apartment(4, 80.0, [])
 building.add_room(empty_apartment)
-
 # Пытаемся рассчитать ежемесячную оплату для квартиры без жильцов
 print(empty_apartment.calculate_monthly_payment())
-
